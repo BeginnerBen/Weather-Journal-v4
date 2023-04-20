3382b3cb7e7694e4635932c97a2f755c06f6cce0
BeginnerBen <benreaby100@gmail.com>
2023-04-20 15:57:02 +1000

    Add files via upload

---
+++ weather-journal-app/server.js
@@ -0,0 +1,54 @@
+/* website localhost:3000 works */
+/* apt get works for localhost:3000/all */ 
+
+/* website localhost:3000 works */
+/* apt get works for localhost:3000/all */
+/* post works on post man  */ 
+
+/* Empty JS object to act as endpoint for all routes */
+projectData = {name: "ben"};
+
+/* Express to run server and routes */
+const express = require('express');
+
+/* Start up an instance of app */
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+/* Initialize the main project folder*/
+app.use(express.static('website'));
+
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+
+app.get('/all', sendData);
+
+
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+app.post('/post', function (req, res) {
+  res.send('POST received');
+});
+
+const data = [{movie: "heritory"}];
+const body = [{movie:"body"}];
+
+app.post('/addAnimal', function (req, res){
+  console.log(body);
+  data.push(body);
+  res.send(data);
+});
---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,25 @@
+/* Function to POST data */
+const apiKey = 'YOUR_API_KEY_HERE';
+
+const postData = async ( url = '', data = {})=>{
+  console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+}
+
+postData('http://localhost:3000/website/addAnimals', {animal:'lion'});
